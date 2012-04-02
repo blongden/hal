8eb7358921d915881a000bc8a0a1fdd1e784d20e
Ben Longden <ben@ibuildings.com>
2012-04-02 14:49:54 +0100

    Updated docs - now generated in phpdoc

--- src/Nocarrier/Hal.php
+++ src/Nocarrier/Hal.php
@@ -1,20 +1,71 @@
 <?php
+/**
+ * This file is part of the Hal library
+ *
+ * (c) Ben Longden <ben@nocarrier.co.uk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Nocarrier
+ */
+
 namespace Nocarrier;
 
+/**
+ * The Hal document class
+ *
+ * @package Nocarrier
+ * @author Ben Longden <ben@nocarrier.co.uk>
+ */
 class Hal
 {
+    /**
+     * The URI of the current resource
+     *
+     * @var mixed
+     * @access protected
+     */
     protected $uri;
 
+    /**
+     * Any related links
+     *
+     * @var mixed
+     * @access protected
+     */
     protected $links = array();
 
+    /**
+     * An array of resources to be embedded in this representation
+     *
+     * @var mixed
+     * @access protected
+     */
     protected $resources = array();
 
+    /**
+     * Construct a new Hal representation representing the resource found at $uri
+     *
+     * @param mixed $uri
+     * @access public
+     * @return void
+     */
     public function __construct($uri)
     {
         // TODO: validate uri
         $this->uri = $uri;
     }
 
+    /**
+     * Add an associated link to the current resource, identified by $ref and found at $uri. $title is optional.
+     *
+     * @param mixed $rel
+     * @param mixed $uri
+     * @param mixed $title
+     * @access public
+     * @return void
+     */
     public function addLink($rel, $uri, $title = null)
     {
         // TODO: validate uri
@@ -24,11 +75,27 @@ class Hal
         );
     }
 
+    /**
+     * Add an embedded resource, identified by $rel and represented by $resource.
+     *
+     * @param mixed $rel
+     * @param HalResource $resource
+     * @access public
+     * @return void
+     */
     public function addResource($rel, HalResource $resource)
     {
         $this->resources[$rel][] = $resource;
     }
 
+    /**
+     * Return an array (compatible with the hal+json format) representing associated links
+     *
+     * @param mixed $uri
+     * @param mixed $links
+     * @access protected
+     * @return array
+     */
     protected function linksForJson($uri, $links)
     {
         $data = array(
@@ -48,6 +115,15 @@ class Hal
         return $data;
     }
 
+    /**
+     * Return an array (compatible with the hal+json format) representing associated resources
+     *
+     * @param mixed $rel
+     * @param mixed $resources
+     * @param array $data
+     * @access protected
+     * @return array
+     */
     protected function resourcesForJson($rel, $resources, $data = array())
     {
         foreach($resources as $resource) {
@@ -67,7 +143,14 @@ class Hal
         return $data;
     }
 
-    public function asJson()
+    /**
+     * asJson
+     * Return the current object in a application/hal+json format (links and resources)
+     *
+     * @access public
+     * @return string
+     */
+    public function asJson($pretty=true)
     {
         $data = $this->linksForJson($this->uri, $this->links);
 
@@ -76,13 +159,22 @@ class Hal
         }
 
         $options = 0;
-        if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+        if (version_compare(PHP_VERSION, '5.4.0') >= 0 and $pretty) {
             $options = JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT;
         }
         return json_encode($data, $options);
     }
 
-    protected function linksForXml($doc, $links)
+    /**
+     * linksForXml
+     * Add links in hal+xml format to a SimpleXmlElement object
+     *
+     * @param SimpleXmlElement $doc
+     * @param array $links
+     * @access protected
+     * @return void
+     */
+    protected function linksForXml(\SimpleXmlElement $doc, array $links)
     {
         foreach($links as $rel => $link) {
             $element = $doc->addChild('link');
@@ -94,7 +186,17 @@ class Hal
         }
     }
 
-    protected function resourcesForXml($doc, $rel, $resources)
+    /**
+     * resourcesForXml
+     * Add resources in hal+xml format (identified by $rel) to a SimpleXmlElement object
+     *
+     * @param SimpleXmlElement $doc
+     * @param mixed $rel
+     * @param array $resources
+     * @access protected
+     * @return void
+     */
+    protected function resourcesForXml(\SimpleXmlElement $doc, $rel, array $resources)
     {
         foreach($resources as $resource) {
             $element = $doc->addChild('resource');
@@ -122,7 +224,14 @@ class Hal
         }
     }
 
-    public function asXml()
+    /**
+     * asXml
+     * Return the current object in a application/hal+xml format (links and resources)
+     *
+     * @access public
+     * @return void
+     */
+    public function asXml($pretty=false)
     {
         $doc = new \SimpleXMLElement('<resource></resource>');
         $doc->addAttribute('href', $this->uri);
@@ -133,8 +242,10 @@ class Hal
         }
 
         $dom = dom_import_simplexml($doc);
-        $dom->ownerDocument->preserveWhiteSpace = false;
-        $dom->ownerDocument->formatOutput = true;
+        if ($pretty) {
+            $dom->ownerDocument->preserveWhiteSpace = false;
+            $dom->ownerDocument->formatOutput = true;
+        }
         return $dom->ownerDocument->saveXML();
     }
 }
--- src/Nocarrier/HalResource.php
+++ src/Nocarrier/HalResource.php
@@ -1,13 +1,46 @@
 <?php
+/**
+ * This file is part of the Hal library
+ *
+ * (c) Ben Longden <ben@nocarrier.co.uk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Nocarrier
+ */
 
 namespace Nocarrier;
 
+/**
+ * The Hal resource class
+ *
+ * @uses Hal
+ * @package Nocarrier
+ * @author Ben Longden <ben@nocarrier.co.uk>
+ */
 class HalResource extends Hal
 {
-    protected $rel;
-
+    /**
+     * The data for this resource. An associative array of key value pairs.
+     * array(
+     *     'price' => 30.00,
+     *     'colour' => 'blue'
+     * )
+     *
+     * @var array
+     * @access protected
+     */
     protected $data;
 
+    /**
+     * construct a new HalResource. Call the parent and store the additional data on the resource.
+     *
+     * @param mixed $uri
+     * @param array $data
+     * @access public
+     * @return void
+     */
     public function __construct($uri, array $data = array())
     {
         parent::__construct($uri);
