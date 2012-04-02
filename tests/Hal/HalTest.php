d23e196905bfa4a398387145a8f9713361124d21
Ben Longden <ben@ibuildings.com>
2012-04-02 19:08:17 +0100

    Don't pretty print by default

--- tests/Hal/HalTest.php
+++ tests/Hal/HalTest.php
@@ -30,13 +30,13 @@ class HalTest extends \PHPUnit_Framework_TestCase
     public function testHalResponseReturnsMinimalValidJson()
     {
         $h = new Hal('http://example.com/');
-        $this->assertEquals('{"_links":{"self":{"href":"http:\/\/example.com\/"}}}', $h->asJson(false));
+        $this->assertEquals('{"_links":{"self":{"href":"http:\/\/example.com\/"}}}', $h->asJson());
     }
 
     public function testHalResponseReturnsMinimalValidXml()
     {
         $h = new Hal('http://example.com/');
-        $this->assertEquals("<?xml version=\"1.0\"?>\n<resource href=\"http://example.com/\"/>\n", $h->asXml(false));
+        $this->assertEquals("<?xml version=\"1.0\"?>\n<resource href=\"http://example.com/\"/>\n", $h->asXml());
     }
 
     public function testAddLinkJsonResponse()
