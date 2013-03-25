23939fb57baab95de007e19cd3af5b4f91613dec
Rowan Merewood <rowan@merewood.org>
2013-03-25 09:50:49 +0000

    Convert strings to HAL
    Added a factory class to build HAL instances from XML and JSON formats

---
+++ src/Nocarrier/Exception/InvalidHalException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Hal library
+ *
+ * (c) Ben Longden <ben@nocarrier.co.uk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Nocarrier
+ */
+
+namespace Nocarrier\Exception;
+
+/**
+ * Exception indicating that the HAL representation being processed is not valid.
+ */
+class InvalidHalException extends \RuntimeException
+{
+}
