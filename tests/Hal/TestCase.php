634dfb9025c78dc75438a61cf5e1c91aadcb558b
Jan Sorgalla <jsorgalla@gmail.com>
2020-04-18 14:42:58 +0200

    Make PHPUnit compatible with PHP 5.3 through 7.4

---
+++ tests/Hal/TestCase.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the Hal library
+ *
+ * (c) Ben Longden <ben@nocarrier.co.uk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Nocarrier
+ */
+
+namespace Nocarrier\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    public function setExpectedException($exception, $exceptionMessage = '', $exceptionCode = null)
+    {
+        if (method_exists($this, 'expectException')) {
+            // PHPUnit 5+
+            $this->expectException($exception);
+            if ($exceptionMessage !== '') {
+                $this->expectExceptionMessage($exceptionMessage);
+            }
+            if ($exceptionCode !== null) {
+                $this->expectExceptionCode($exceptionCode);
+            }
+        } else {
+            // legacy PHPUnit 4
+            parent::setExpectedException($exception, $exceptionMessage, $exceptionCode);
+        }
+    }
+}
